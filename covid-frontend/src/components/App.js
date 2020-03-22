ebe5cbf53d43c6557b5c74708fbd2f65c048efdd
Oliver Schafeld <oliver@schafeld.com>
2020-03-22 17:56:08 +0100

    remove Ant Design config provider

--- covid-frontend/src/components/App.js
+++ covid-frontend/src/components/App.js
@@ -1,15 +1,11 @@
 import React, { Component, Fragment } from 'react';
-import { ConfigProvider } from 'antd';
-import deDE from 'antd/es/locale/de_DE';
 
 class App extends Component {
 
   render () {
     return (
       <Fragment>
-        <ConfigProvider locale={deDE}>
-          { this.props.children }
-        </ConfigProvider>
+        { this.props.children }
       </Fragment>
     );
   }
